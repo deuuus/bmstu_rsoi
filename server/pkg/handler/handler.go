de212e5296d408e8119140ae41bedc008c1d8034
deuuus <sirotkina.polina.2001@gmail.com>
2023-09-26 20:51:11 +0300

    add cors

--- server/pkg/handler/handler.go
+++ server/pkg/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/deuuus/bmstu-rsoi/pkg/service"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 type Handler struct {
@@ -14,7 +15,14 @@ func NewHandler(services *service.Service) *Handler {
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
-	router := gin.New()
+	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowHeaders = []string{"token", "Origin", "X-Requested-With", "Content-Type", "Accept"}
+	config.AllowMethods = []string{"GET", "POST", "PATCH", "DELETE"}
+
+	router.Use(cors.New(config))
 
 	api := router.Group("/api/v1")
 	{
