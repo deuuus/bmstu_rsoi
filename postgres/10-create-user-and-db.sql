ea7a4591c36fcea5ec50aac196ba6f33fce12c55
deuuus <sirotkina.polina.2001@gmail.com>
2023-09-27 11:01:04 +0300

    add postgres script

--- postgres/10-create-user-and-db.sql
+++ postgres/10-create-user-and-db.sql
@@ -1,5 +1,16 @@
 -- file: 10-create-user-and-db.sql
 CREATE DATABASE persons;
+
 CREATE ROLE program WITH PASSWORD 'test';
 GRANT ALL PRIVILEGES ON DATABASE persons TO program;
 ALTER ROLE program WITH LOGIN;
+
+\c persons;
+
+CREATE TABLE persons(
+    id serial PRIMARY KEY,
+    name VARCHAR (64),
+    age INT,
+    address VARCHAR(128),
+    work VARCHAR(128)
+);
