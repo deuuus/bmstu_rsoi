db4daa92d68373ad98adc1a9639428a503f462a3
deuuus <sirotkina.polina.2001@gmail.com>
2023-09-28 11:18:56 +0300

    add debug print

--- server/pkg/repository/postgres.go
+++ server/pkg/repository/postgres.go
@@ -19,12 +19,12 @@ type Config struct {
 func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
 	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
-
-	logrus.Infof("here")
 	
 	if err != nil {
 		return nil, err
 	}
+	
+	logrus.Infof("here")
 
 	err = db.Ping()
 	if err != nil {
